a2d248cf9f059ceb8b42c3404f95b5f56db799d0
Syrus Nelson <syrus.n.career@gmail.com>
2023-09-10 13:33:37 -0700

    Fixed bug on evaluating points greater than 4, or less than -4.

--- runEvaluation.js
+++ runEvaluation.js
@@ -13,36 +13,54 @@ export async function runEvaluation() {
    
    var result = await compareTypes(poke1type1, poke1type2, poke2type1, poke2type2);
    console.log(result);
-   if (result == -4) {
+   if (result <= -4) {
+
       var outcome = document.getElementById("outcome");
       outcome.innerHTML = "Pokémon 1 will lose this battle.";
+   
    } else if (result == -3) {
+   
       var outcome = document.getElementById("outcome");
       outcome.innerHTML = "Pokémon 1 will probably lose this battle.";
+   
    } else if (result == -2) {
+   
       var outcome = document.getElementById("outcome");
       outcome.innerHTML = "Pokémon 1 has a great chance of losing this battle.";
+   
    } else if (result == -1) {
+   
       var outcome = document.getElementById("outcome");
       outcome.innerHTML = "Pokémon 1 has a moderate chance of losing this battle.";
+   
    } else if (result == 0){
+   
       var outcome = document.getElementById("outcome");
       outcome.innerHTML = "Either Pokémon can win the battle.";
+   
    } else if (result == 1) {
+   
       var outcome = document.getElementById("outcome");
       outcome.innerHTML = "Pokémon 1 has a moderate chance of winning this battle.";
+   
    } else if (result == 2) {
+   
       var outcome = document.getElementById("outcome");
       outcome.innerHTML = "Pokémon 1 has a great chance of winning this battle.";
+   
    } else if( result == 3) {
+   
       var outcome = document.getElementById("outcome");
       outcome.innerHTML = "Pokémon 1 will probably win this battle.";
-   } else if (result == 4) {
+   
+   } else if (result >= 4) {
+   
       var outcome = document.getElementById("outcome");
       outcome.innerHTML = "Pokémon 1 will win this battle.";
+   
    } else {
       alert("Error");
    }
-   console.log("Result: ", result);
+   // console.log("Result: ", result);
 }
 runEvaluation();
