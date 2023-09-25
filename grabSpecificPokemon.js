e543ce538746f9433fcc562797565af60a89c471
SyrusN <70251133+SyrusN@users.noreply.github.com>
2023-09-24 21:12:49 -0700

    Working (#8)
    * Removed extra page.
    
    * Added notice to html for reasons of IP.
    
    * Added functionality to allow the user to enter their own Pokemon for comparison. Also heavily refactored the code.
    
    * Revised README.md
    
    * Added more comments to help understanding of the code.
    
    ---------
    
    Co-authored-by: Syrus Nelson <syrus.n.career@gmail.com>

--- grabSpecificPokemon.js
+++ grabSpecificPokemon.js
@@ -1,5 +1,6 @@
 import {setImage, jsonPokemonData, setTypings} from "/setData.js";
 export async function setSpecificInfo(pokemonName, slotNum) {
+   //Get the pokemon name for a given slot number
    var pName = document.getElementById(`p${slotNum}Name`);
   
    var pData = await jsonPokemonData(pokemonName);
